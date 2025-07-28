eb68b910953de1b750be98a531b4b5c7b6ae802d
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2025-07-28 09:50:48 -0300

    feat(server): :sparkles: iniciar servidor com rota basica

--- monitoramento-de-temperatura/server/app.js
+++ monitoramento-de-temperatura/server/app.js
@@ -1,1 +1,16 @@
-console.log("test")
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor rodando!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
