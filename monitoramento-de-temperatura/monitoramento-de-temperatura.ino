bf340e55aa62bdfdb2fcd71c41c34a1bd200ec71
Paula Ribeiro <caped.20201p2ads0226@aluno.ifpi.edu.br>
2025-08-06 20:46:22 -0300

    feat(LCD 1602 I2C): integração com o arquivo principal - pegando os dados capturado pelo sensor para exibição no display

--- monitoramento-de-temperatura/monitoramento-de-temperatura.ino
+++ monitoramento-de-temperatura/monitoramento-de-temperatura.ino
@@ -1,10 +1,11 @@
-// Código para leitura de temperatura e umidade usando o sensor AHT10 com ESP32
+// Código para leitura de temperatura e umidade usando o sensor AHT10 com ESP8266 D1 MINI
 // Os dados são exibidos no monitor serial a cada 3 segundos
 
 // ===== INCLUDES E DEFINIÇÕES =====
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include "Adafruit_AHTX0.h"
+#include "display_LCD-1602_I2C.h"
 
 #define SSID "XXXXXXX"
 #define PASSWORD "XXXXXXXX"
@@ -51,6 +52,12 @@ void printSensorData(const EnvironmentData& data) {
   Serial.print("Umidade: ");
   Serial.print(data.humidity);
   Serial.println(" %");
+}
+
+// Chama a função de mostrar os dados de temperatura na tela
+void printDataDisplay(const EnvironmentData& data) {
+  // Passa os dados e os status de alerta para a função de exibição
+  show_data_tempHum_DisplayLCD_1602_I2C(data.temperature, data.humidity, false, false);
 }
 
 bool conectWiFi() {
@@ -102,6 +109,8 @@ void setup() {
   while (!Serial) delay(10);
 
   initializeSensor();
+  // Chama a função para iniciar o display
+  initializeDisplayLCD_1602_I2C();
 
   if(!conectWiFi()){
 
@@ -116,6 +125,8 @@ void loop() {
   EnvironmentData sensorReading = readSensorData();
   if (sensorReading.valid) {
     printSensorData(sensorReading);
+    // Exibir dados no display
+    printDataDisplay(sensorReading);
   }
   delay(1000);
 }
