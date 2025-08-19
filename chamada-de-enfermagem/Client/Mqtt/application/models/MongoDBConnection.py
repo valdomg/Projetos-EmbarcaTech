e169027d84331f5d3cb580490b014b6b34b7b506
Valdemiro <valdemiro128@outlook.com>
2025-08-19 11:26:56 -0300

    :recycle: refactor(MongoDBConnection): Blocos de try except para tratamento de erros no Crud

--- chamada-de-enfermagem/Client/Mqtt/application/models/MongoDBConnection.py
+++ chamada-de-enfermagem/Client/Mqtt/application/models/MongoDBConnection.py
@@ -1,5 +1,5 @@
  
-from pymongo.mongo_client import MongoClient
+from pymongo.mongo_client import MongoClient, PyMongoError, ConnectionFailure
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
@@ -23,58 +23,71 @@ class MongoDBConnection:
     Função de iniciar a conexão com o banco de dados
     '''
     def start_connection(self):
-        self.client = MongoClient(self.uri, server_api=ServerApi('1'))
-        self.db = self.client[self.database_name]
-
+        
         try:
+            self.client = MongoClient(self.uri, server_api=ServerApi('1'))
+            self.db = self.client[self.database_name]
             self.client.admin.command('ping')
             print("Pinged your deployment. You successfully connected to MongoDB!")
-        except Exception as e:
+        except ConnectionFailure as e:
             self.client = None
             self.db = None
+            print('Error on conennection...')
             print(e)
+        
     
     '''
     Insere um novo documento na coleção
     '''
     def insert_document_collection(self,collection:str, local:str, room_number:int):
 
-        if self.client is not None:
-            document = {
-                local: room_number,
-                'data': datetime.now() 
-            }
+        try:
+            if self.client is not None:
+                document = {
+                    local: room_number,
+                    'data': datetime.now() 
+                }
 
-            collection = self.db[self.collection]
-            result = collection.insert_one(document)
+                collection = self.db[self.collection]
+                result = collection.insert_one(document)
 
-            print(result)
+                print(result)
 
-        else:
-            print('Client not connected')
+            else:
+                print('Client not connected')
 
+        except PyMongoError as e:
+            print('Error on insert document...')
+            print(e)
     '''
     Lista os documentos na coleção
     '''
     def list_documents(self):
-        if self.client is not None:
-            collection = self.db[self.collection]
-            return list(collection.find())
-        else:
-            print('Client not connected')
-
+        try:
+            if self.client is not None:
+                collection = self.db[self.collection]
+                return list(collection.find())
+            else:
+                print('Client not connected')
+        except PyMongoError as e:
+            print('Error in list documents...')
+            print(e)
     '''
     Verifica a existência de algum dado no banco de dados
     '''
     def check_if_document_exists(self, collection:str, device:str):
-        if self.client is not None:
-            collection = self.db[collection]
 
-            result = collection.find_one({"device": device})
+        try:
+            if self.client is not None:
+                collection = self.db[collection]
 
-            if result is not None:
-                return True
-            
+                result = collection.find_one({"device": device})
+
+                if result is not None:
+                    return True
+        except PyMongoError as e:
+            print('Error in check values...')
+            print(e)    
         return False
     
     '''
