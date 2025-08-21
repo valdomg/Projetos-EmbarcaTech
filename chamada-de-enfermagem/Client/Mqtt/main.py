143b29881d6f635f41c7f4bbaf5917a6a305dde4
Valdemiro <valdemiro128@outlook.com>
2025-08-21 10:54:22 -0300

    :recycle: refactor(Main): adicionando variáveis .dotenv para instaciação da classe MqttClientConnection

--- chamada-de-enfermagem/Client/Mqtt/main.py
+++ chamada-de-enfermagem/Client/Mqtt/main.py
@@ -1,7 +1,18 @@
 
 from application.controller.mqtt_client_connection import MqttClientConnection
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+broker = os.getenv('BROKER_IP')
+port = int(os.getenv('BROKER_PORT'))
+topic = os.getenv('BROKER_TOPIC')
+client_name = os.getenv('CLIENT_NAME')
+
+print(broker, port, topic)
 
 if __name__ == '__main__':
-    mqttclient = MqttClientConnection()
+    mqttclient = MqttClientConnection(broker, port, client_name, 60)
     mqttclient.start()
     
