c4fe54e988b8d9f83850880ccaf7094208127a29
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-09-01 12:53:35 -0300

    :truck: movendo arquivos front-end para a pasta do client Flask

---
+++ chamada-de-enfermagem/Client/Flask/static/js/script.js
@@ -0,0 +1,19 @@
+const socket = io("http://localhost:3000");
+
+const chamadasLista = document.getElementById("chamadas-lista");
+
+function adicionarChamada(chamada) {
+    const div = document.createElement("div");
+    div.className = "row border rounded mb-3";
+    div.textContent = `Chamada: Enfermaria ${chamada.enfermaria}; ${chamada.horario}.`;
+    chamadasLista.prepend(div); 
+}
+
+
+socket.on("todas-chamadas", (chamadas) => {
+    chamadas.forEach(chamada => adicionarChamada(chamada));
+});
+
+socket.on("nova-chamada", (chamada) => {
+    adicionarChamada(chamada);
+});
