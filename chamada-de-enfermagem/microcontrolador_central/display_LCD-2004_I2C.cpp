64e44c04b4a4e0d0b58a152a80efec76e83e0a49
Paula Ribeiro <caped.20201p2ads0226@aluno.ifpi.edu.br>
2025-09-07 11:04:33 -0300

    feat(LCD 2004 I2C): implementa a funcionalidade de limpar à área no display.

--- chamada-de-enfermagem/microcontrolador_central/display_LCD-2004_I2C.cpp
+++ chamada-de-enfermagem/microcontrolador_central/display_LCD-2004_I2C.cpp
@@ -3,7 +3,6 @@
 #include "display_LCD-2004_I2C.h"  // Inclusão do arquivo de cabeçalho do módulo que contém as declarações de funções
 
 
-
 const uint8_t I2C_ADDR = 0x27;  // Endereço padrão do módulo I2C do LCD
 // Define quantidade de linhas e colunas do LCD (20 colunas e 4 linhas)
 const uint8_t LCD_COLUMNS = 20;
@@ -14,18 +13,40 @@ const uint8_t LCD_LINES = 4;
 LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 
 
+/*Função de Limpeza de Linha
+    Parâmetro:
+      - column: coluna do display a ser limpa (uint8_t)
+      - line: linha do display a ser limpa (uint8_t) 
+      - count: Quant. colunas a ser limpas (uint8_t)*/
+void clearLine(uint8_t column, uint8_t line, uint8_t count) {
+  if (line >= LCD_LINES || column >= LCD_COLUMNS) return;  // valida linha e coluna
+  // Posiciona o cursor na área escolhida
+  lcd.setCursor(column, line);
+  // Imprime espaços para apagar o conteúdo anterior
+  for (uint8_t i = 1; i <= count; i++) {
+    lcd.print(F(" "));
+  }
+}
+
+
 // Função que mostra os dados no display
 void show_infirmary_numberCalls(List_NursingCall &listCalls) {
+  // Limpa 8 espaços na coluna/linha escolhida - ENFERMARIA
+  clearLine(12, 2, 8);
+
+  // Limpa 3 espaços na coluna/linha escolhida - QUANT. CHAMADAS
+  clearLine(17, 3, 3);
+
+
   // Linha 3 - mostrando o número da enfermaria
   lcd.setCursor(12, 2);
   if (listCalls.hasNursingCall()) {
-
-    lcd.print("< ");
+    lcd.print(F("< "));
     lcd.print(listCalls.getInfirmaryCurrent());
-    lcd.print(" >");
+    lcd.print(F(" >"));
 
   } else {
-    lcd.print(" X      ");
+    lcd.print(F(" X"));
   }
 
   // Linha 4 - mostrando a quantidade de chamados não resolvidos
--- chamada-de-enfermagem/microcontrolador_central/microcontrolador_central.ino
+++ chamada-de-enfermagem/microcontrolador_central/microcontrolador_central.ino
@@ -16,13 +16,6 @@ void setup() {
   // inicializa o display
   lcd2004_init();
 
-  // Teste de adição de valores
-  listCalls.add(10);
-  listCalls.add(20);
-  listCalls.add(15);
-  listCalls.add(11);
-
-  show_infirmary_numberCalls(listCalls);
 }
 
 void loop() {
