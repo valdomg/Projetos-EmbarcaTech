e8f2548e873d2a0ca9e7a8416f64a0839e4231d5
Pedro Alysson <pedro.alysson@hotmail.com>
2025-09-09 23:15:25 -0300

    style: altera cores de componentes para melhorar aparência

--- monitoramento-de-temperatura/interface/src/js/gauge.js
+++ monitoramento-de-temperatura/interface/src/js/gauge.js
@@ -1,53 +1,58 @@
 
-export function createGauge(tempId, UmidId, dado){
+export function createGauge(tempId, umidId, dado) {
 
-new JustGage({
-  id: tempId, // the id of the html element
-  value: dado.temperature,
-  min: 0,
-  max: 100,
-  decimals: 0,
-  symbol: '°c',
-  gaugeWidthScale: 0.9,
-  title: "Lone Ranger",
-  // label: "Temperatura",
-  pointer: "true",
-  pointerOptions: {
-    toplength: 0,
-    bottomlength: 15,
-    bottomwidth: 3,
-    stroke: 'none',
-    stroke_width: 0,
-    stroke_linecap: 'square',
-    color: 'dark'
-  }
-});
+    new JustGage({
 
-// // update the value randomly
-// setInterval(() => {
-//   gauge.refresh(Math.random() * 100);
-// }, 5000)
+        id: tempId, // the id of the html element
+        value: dado.temperature,
+        min: 0,
+        max: 50,
+        decimals: 0,
+        symbol: '°c',
+        gaugeWidthScale: 1,
+        title: "Lone Ranger",
+        // label: "Temperatura",
+        pointer: "true",
+        pointerOptions: {
+            toplength: 0,
+            bottomlength: 15,
+            bottomwidth: 3,
+            stroke: 'none',
+            stroke_width: 0,
+            stroke_linecap: 'square',
+            color: 'dark'
+        },
+        levelColors: ["#069625ff", "#d9ff00ff", "#ff0000ff"],
+        gaugeColor: "rgba(219, 216, 216, 1)",
+    });
 
-new JustGage({
-  id: UmidId, // the id of the html element
-  value: dado.humidity,
-  min: 0,
-  max: 100,
-  decimals: 0,
-  gaugeWidthScale: 0.9,
-  symbol: '%',
-  // label: "Umidade",
-  pointer: "true",
-  pointerOptions: {
-    toplength: 0,
-    bottomlength: 15,
-    bottomwidth: 3,
-    stroke: 'none',
-    stroke_width: 0,
-    stroke_linecap: 'square',
-    color: 'dark'
-  },
-  levelColors: ["#9bd4faff", "#008cffff", "#0400ffff"]
-});
+    // // update the value randomly
+    // setInterval(() => {
+    //   gauge.refresh(Math.random() * 100);
+    // }, 5000)
+
+    new JustGage({
+
+        id: umidId, // the id of the html element
+        value: dado.humidity,
+        min: 0,
+        max: 100,
+        decimals: 0,
+        gaugeWidthScale: 1,
+        symbol: '%',
+        // label: "Umidade",
+        pointer: "true",
+        pointerOptions: {
+            toplength: 0,
+            bottomlength: 15,
+            bottomwidth: 3,
+            stroke: 'none',
+            stroke_width: 0,
+            stroke_linecap: 'square',
+            color: 'dark'
+        },
+        levelColors: ["#9bd4faff", "#008cffff", "#0400ffff"],
+        gaugeColor: "rgba(219, 216, 216, 1)",
+    });
 
 }
