ba7e0ede3311b7cb1ce77b598715160edcee39d5
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2025-09-10 13:55:09 -0300

    feat: :sparkles: adicionando verificação de acesso para administradores nas funções de criação, atualização e exclusão de ambientes e usuários

--- monitoramento-de-temperatura/server/controllers/UserControllers.js
+++ monitoramento-de-temperatura/server/controllers/UserControllers.js
@@ -2,11 +2,11 @@ import User from "../models/User.js";
 import UserService from "../services/User/UserServices.js";
 
 class UserController {
-  constructor() {
-    this.userService = new UserService(User);
-  }
+    constructor() {
+        this.userService = new UserService(User);
+    }
 
-  createUser = async (req, res) => {
+    createUser = async (req, res) => {
         /*
           #swagger.tags = ['Users']
           #swagger.summary = 'cria um novo usuário'
@@ -16,46 +16,51 @@ class UserController {
           }] 
         
           */
-    const userData = req.body;
-    /*  #swagger.requestBody = {
-                required: true,
-                content: {
-                    "application/json": {
-                        schema: {
-                            type: "object",
-                            properties: {
-                                name: { type: "string", example: "João Silva", required: true },
-                                email: { type: "string", example: "joao.silva@example.com", required: true },
-                                password: { type: "string", example: "SenhaSegura123", required: true }
+        const userData = req.body;
+        /*  #swagger.requestBody = {
+                    required: true,
+                    content: {
+                        "application/json": {
+                            schema: {
+                                type: "object",
+                                properties: {
+                                    name: { type: "string", example: "João Silva", required: true },
+                                    email: { type: "string", example: "joao.silva@example.com", required: true },
+                                    password: { type: "string", example: "SenhaSegura123", required: true }
+                                }
                             }
                         }
                     }
                 }
-            }
-    */
-
-
-    const user = await this.userService.createUser(userData);
-    res.status(201).json(user);
-    /*  #swagger.responses[201] = {
-                description: "Usuário criado com sucesso",
-                content: {
-                    "application/json": {
-                        schema:{
-                            type: "object",
-                            properties: {
-                                _id: { type: "string", example: "64a7b2f5e4b0c8a1d2f3g4h5" },
-                                name: { type: "string", example: "João Silva" },
-                                email: { type: "string", example: "joao.silva@example.com" }
-                            }
-                        }
-                    }           
-                }
-            }   
         */
-  }
 
-  getUserByEmail = async (req, res) => {
+        const { role } = req.user;
+        if (role !== 'admin') {
+            return res.status(403).json({ message: 'Acesso negado. Apenas administradores podem criar ambientes.' });
+        }
+
+
+        const user = await this.userService.createUser(userData);
+        res.status(201).json(user);
+        /*  #swagger.responses[201] = {
+                    description: "Usuário criado com sucesso",
+                    content: {
+                        "application/json": {
+                            schema:{
+                                type: "object",
+                                properties: {
+                                    _id: { type: "string", example: "64a7b2f5e4b0c8a1d2f3g4h5" },
+                                    name: { type: "string", example: "João Silva" },
+                                    email: { type: "string", example: "joao.silva@example.com" }
+                                }
+                            }
+                        }           
+                    }
+                }   
+            */
+    }
+
+    getUserByEmail = async (req, res) => {
         /*
           #swagger.tags = ['Users']
           #swagger.summary = 'Busca um usuário pelo email'
@@ -64,41 +69,41 @@ class UserController {
             "bearerAuth": []
           }]         
           */
-    const { email } = req.params;
-    const user = await this.userService.getUserByEmail(email);
-    res.status(200).json(user);
-    /*  #swagger.responses[200] = {
-                description: "Usuário encontrado com sucesso",
-                content: {
-                    "application/json": {
-                        schema:{
-                            type: "object",
-                            properties: {
-                                _id: { type: "string", example: "64a7b2f5e4b0c8a1d2f3g4h5" },
-                                name: { type: "string", example: "João Silva" },
-                                email: { type: "string", example: "joao.silva@example.com" }
+        const { email } = req.params;
+        const user = await this.userService.getUserByEmail(email);
+        res.status(200).json(user);
+        /*  #swagger.responses[200] = {
+                    description: "Usuário encontrado com sucesso",
+                    content: {
+                        "application/json": {
+                            schema:{
+                                type: "object",
+                                properties: {
+                                    _id: { type: "string", example: "64a7b2f5e4b0c8a1d2f3g4h5" },
+                                    name: { type: "string", example: "João Silva" },
+                                    email: { type: "string", example: "joao.silva@example.com" }
+                                }
                             }
-                        }
-                    }           
+                        }           
+                    }
                 }
-            }
-           #swagger.responses[400] = {
-                description: "Usuário não encontrado",
-                content: {
-                    "application/json": {
-                        schema:{
-                            type: "object",
-                            properties: {
-                                error: { type: "string", example: "Usuário não encontrado" }
+               #swagger.responses[400] = {
+                    description: "Usuário não encontrado",
+                    content: {
+                        "application/json": {
+                            schema:{
+                                type: "object",
+                                properties: {
+                                    error: { type: "string", example: "Usuário não encontrado" }
+                                }
                             }
                         }
                     }
-                }
-            }      
-        */
-  };
+                }      
+            */
+    };
 
-  getAllUsers = async (req, res) => {
+    getAllUsers = async (req, res) => {
         /*
           #swagger.tags = ['Users']
           #swagger.summary = 'Busca todos os usuários'
@@ -107,74 +112,79 @@ class UserController {
             "bearerAuth": []
           }] 
           */
-    const users = await this.userService.getAllUsers();
-    res.status(200).json(users);
-    /*  #swagger.responses[200] = {
-                description: "Lista de usuários retornada com sucesso",
-                content: {
-                    "application/json": {
-                        schema:{
-                            type: "array",
-                            items: {
-                                type: "object",
-                                properties: {
-                                    _id: { type: "string", example: "64a7b2f5e4b0c8a1d2f3g4h5" },
-                                    name: { type: "string", example: "João Silva" },
-                                    email: { type: "string", example: "joao.silva@example.com" }
+        const users = await this.userService.getAllUsers();
+        res.status(200).json(users);
+        /*  #swagger.responses[200] = {
+                    description: "Lista de usuários retornada com sucesso",
+                    content: {
+                        "application/json": {
+                            schema:{
+                                type: "array",
+                                items: {
+                                    type: "object",
+                                    properties: {
+                                        _id: { type: "string", example: "64a7b2f5e4b0c8a1d2f3g4h5" },
+                                        name: { type: "string", example: "João Silva" },
+                                        email: { type: "string", example: "joao.silva@example.com" }
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-        */
-  }
+            */
+    }
 
-  updateUser = async (req, res) => {
-    /*
-      #swagger.tags = ['Users']
-      #swagger.summary = 'Atualiza um usuário existente'
-      #swagger.description = 'Esse endpoint atualiza os dados de um usuário existente no sistema. É necessário fornecer um ID de usuário válido como parâmetro e os novos dados no corpo da requisição.'
-          #swagger.security = [{
-            "bearerAuth": []
-          }] 
-      */
-    const { userId } = req.params;
-    /*  #swagger.requestBody = {
-                required: true,
-                content: {
-                    "application/json": {
-                        schema: {
-                            type: "object",
-                            properties: {
-                                name: { type: "string", example: "João Silva" },
-                                email: { type: "string", example: "joao.silva@example.com" },
-                                password: { type: "string", example: "SenhaSegura123" }
+    updateUser = async (req, res) => {
+        /*
+          #swagger.tags = ['Users']
+          #swagger.summary = 'Atualiza um usuário existente'
+          #swagger.description = 'Esse endpoint atualiza os dados de um usuário existente no sistema. É necessário fornecer um ID de usuário válido como parâmetro e os novos dados no corpo da requisição.'
+              #swagger.security = [{
+                "bearerAuth": []
+              }] 
+          */
+        const { userId } = req.params;
+        /*  #swagger.requestBody = {
+                    required: true,
+                    content: {
+                        "application/json": {
+                            schema: {
+                                type: "object",
+                                properties: {
+                                    name: { type: "string", example: "João Silva" },
+                                    email: { type: "string", example: "joao.silva@example.com" },
+                                    password: { type: "string", example: "SenhaSegura123" }
+                                }
                             }
                         }
                     }
                 }
-            }
-    */
+        */
 
-    const updateData = req.body;
-    const updatedUser = await this.userService.updateUser(userId, updateData);
-    res.status(200).json(updatedUser);
-  }
+        const updateData = req.body;
+        const updatedUser = await this.userService.updateUser(userId, updateData);
+        res.status(200).json(updatedUser);
+    }
 
-  deleteUser = async (req, res) => {
-    /*
-      #swagger.tags = ['Users']
-      #swagger.summary = 'Deleta um usuário existente'
-      #swagger.description = 'Esse endpoint deleta um usuário existente no sistema. É necessário fornecer um ID de usuário válido como parâmetro.'
-          #swagger.security = [{
-            "bearerAuth": []
-          }] 
-      */
-    const { userId } = req.params;
-    await this.userService.deleteUser(userId);
-    res.status(204).send();
-  }
+    deleteUser = async (req, res) => {
+        /*
+          #swagger.tags = ['Users']
+          #swagger.summary = 'Deleta um usuário existente'
+          #swagger.description = 'Esse endpoint deleta um usuário existente no sistema. É necessário fornecer um ID de usuário válido como parâmetro.'
+              #swagger.security = [{
+                "bearerAuth": []
+              }] 
+          */
+
+        const { role } = req.user;
+        if (role !== 'admin') {
+            return res.status(403).json({ message: 'Acesso negado. Apenas administradores podem criar ambientes.' });
+        }
+        const { userId } = req.params;
+        await this.userService.deleteUser(userId);
+        res.status(204).send();
+    }
 }
 
 export default UserController;
