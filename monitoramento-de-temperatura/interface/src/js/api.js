f383458664cb37069feb847fd5c6080179ab02b3
Pedro Alysson <pedro.alysson@hotmail.com>
2025-09-12 12:39:33 -0300

    feat(api): adicionar token de autenticação nas requisições

--- monitoramento-de-temperatura/interface/src/js/api.js
+++ monitoramento-de-temperatura/interface/src/js/api.js
@@ -1,19 +1,51 @@
 
 
 export async function buscarTemperaturas() {
-  const response = await fetch("http://localhost:3000/api/temperatures");
+  const token = localStorage.getItem("token");
+  const response = await fetch("http://localhost:3000/api/temperatures",
+    {headers: {
+       "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`   
+    }
+  });
+
+  if (!response){
+    throw new Error(`Erro na requisição: ${response.status}`);
+  }
   const dados = await response.json();
   return dados;
 }
 
+
 export async function buscarTemperaturaSala(salaId)  {
-    const response = await fetch(`http://localhost:3000/api/room/${salaId}/temperatures`);
-    const dados = await response.json();
-    return dados;
+    const token = localStorage.getItem("token");
+    const response = await fetch(`http://localhost:3000/api/room/${salaId}/temperatures`,
+    {headers: {
+       "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`   
+    }
+  });
+
+  if (!response){
+    throw new Error("Erro ao buscar temperaturas");
+  }
+  const dados = await response.json();
+  return dados;
 }
 
+
 export async function buscarTempIntervalo(salaId, start, end){
-  const response = await fetch(`http://localhost:3000/api/room/${salaId}/temperatures/?start=${start}&end=${end}`);
+  const token = localStorage.getItem("token");
+  const response = await fetch(`http://localhost:3000/api/room/${salaId}/temperatures/?start=${start}&end=${end}`,
+    {headers: {
+       "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`   
+    }
+  });
+
+  if (!response){
+    throw new Error("Erro ao buscar temperaturas");
+  }
   const dados = await response.json();
   return dados;
 }
