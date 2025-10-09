e5e75cde3892a78fa1285369e982e1d13a58c4b0
Valdemiro <valdemiro128@outlook.com>
2025-10-09 14:13:24 -0300

    :sparkles: feat(DeviceDbModel): implementação de funções com base no ID de devices

--- chamada-de-enfermagem/Client/Flask/Models/device_db_model.py
+++ chamada-de-enfermagem/Client/Flask/Models/device_db_model.py
@@ -12,14 +12,17 @@ class DeviceDBModel():
     def find_by_device(self, device:str):
         return self.db.return_document('devices', 'device', device)
     
+    def find_device_by_id(self, document_id:str) -> bool:
+        return self.db.check_if_document_exists_by_id('devices', document_id)
+    
     def insert_device(self, device_data:dict):
         return self.db.insert_document_collection('devices', device_data)
     
     def update_device(self, label_to_match:str, value_to_match:str,label_to_update:str, new_value:str):
         pass
 
-    def delete_device(self, device:str):
-        return self.db.delete_document('devices', 'device', device)
+    def delete_device_by_id(self, document_id:str):
+        return self.db.delete_document_by_id('devices', document_id)
     
     def return_all_devices(self):
         return self.db.list_documents('devices')
