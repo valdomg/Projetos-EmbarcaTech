cc7758fb966c9709186db9cc2988151f395ff5db
Valdemiro <valdemiro128@outlook.com>
2025-10-14 14:55:06 -0300

    :bug: fix(ChamadasRoute): resolvendo bugs em no retorno incorreto de coleções do mongodb

--- chamada-de-enfermagem/Client/Flask/Routes/chamadas_route.py
+++ chamada-de-enfermagem/Client/Flask/Routes/chamadas_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from datetime import datetime
 from Flask.auth import token_required 
+from Flask.Models.chamadas_db_model import ChamadasDBModel
 from Flask.Services.convert_objectdID import convert_all_id_to_string, convert_object_id_to_string
 from Mqtt.application.models.MongoDBConnection import MongoDBConnection
 from dotenv import load_dotenv
@@ -24,6 +25,7 @@ uri = os.getenv('MONGO_URI')
 database = os.getenv('MONGO_DATABASE')
 
 mongo_conn = MongoDBConnection(uri, database)
+chamadas_db_model = ChamadasDBModel(mongo_conn)
 
 chamadas_bp = Blueprint('chamadas', __name__, url_prefix='/api/chamadas')
 
@@ -34,7 +36,7 @@ Rota de api para retorno de todas as chamadas
 def return_all_documents_chamadas():
     mongo_conn.start_connection()
 
-    documents = mongo_conn.list_documents('chamadas')
+    documents = chamadas_db_model.return_all_chamadas()
 
     mongo_conn.close_connection()
 
@@ -49,38 +51,27 @@ Rota de api para retorno das chamadas do dia
 '''
 @chamadas_bp.route('/dia', methods=['GET','POST'])
 def return_documents_chamadas_day():
+
     mongo_conn.start_connection()
 
-    data = datetime.now()
-
-    start_date = f'ISODate({data.year}-0{data.month}-{data.day}T0:00:00.000Z)'
-    end_date = f'ISODate({data.year}-0{data.month}-{data.day}T23:59:59:000Z)'
-
-    print(start_date, end_date)
-
-    documents = mongo_conn.list_documents_by_date('chamadas', 'Data', end_date)
+    chamadas = chamadas_db_model.return_chamadas_by_day()
 
     mongo_conn.close_connection()
-    if documents:
-        return {'sucesso': 'Docs encontrados'}, 201
+    
+    if chamadas:
+
+        json_chamadas = convert_all_id_to_string(chamadas)
+
+        return jsonify(json_chamadas), 201
     
     return {'Erro': 'Docs não encontrados'},400
 
 '''
 Rota de api para contagem de chamadas diárias
-'''
 @chamadas_bp.route('/dia/contagem')
 def return_chamadas_day_count():
+
     mongo_conn.start_connection()
-
-    data = datetime.now()
-
-    start_date = f'ISODate({data.year}-0{data.month}-{data.day}T0:00:00.000Z)'
-    end_date = f'ISODate({data.year}-0{data.month}-{data.day}T23:59:59:000Z)'
-
-    print(start_date, end_date)
-
-    documents = mongo_conn.list_documents_by_date('chamadas', 'Data', end_date)
 
     mongo_conn.close_connection()
 
@@ -89,3 +80,8 @@ def return_chamadas_day_count():
         return {'Quantidade': count_documents}
     
     return {'Erro': 'Docs não encontrados'}, 400
+'''
+
+'''
+
+'''
