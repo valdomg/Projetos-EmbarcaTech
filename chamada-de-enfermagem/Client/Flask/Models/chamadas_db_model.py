560ca0d5e8ee30e346cf02ec0be5e122f7a8a132
Valdemiro <valdemiro128@outlook.com>
2025-10-14 14:52:04 -0300

    :sparkles: feat(ChamadasDBModel): implementação de funções com mongodb

--- chamada-de-enfermagem/Client/Flask/Models/chamadas_db_model.py
+++ chamada-de-enfermagem/Client/Flask/Models/chamadas_db_model.py
@@ -1,5 +1,5 @@
 from Mqtt.application.models.MongoDBConnection import MongoDBConnection
-
+from datetime import datetime, timezone
 
 class ChamadasDBModel():
     
@@ -7,13 +7,21 @@ class ChamadasDBModel():
         self.db = db
 
     def return_all_chamadas(self):
-        return self.db.list_all_documents()
+        return self.db.list_all_documents_from_collection('chamadas')
     
     def return_chamadas_by_day(self):
-        pass
+        date_now = datetime.now()
+        start_date = date_now.replace(hour=0, minute=00, second=00, microsecond=0)
+        end_date = date_now.replace(hour=23, minute=59, second=00, microsecond=9999)
+        
+        return self.db.list_documents_by_date('chamadas', 'Data', start_date, end_date)
+
+    def return_number_of_chamadas_by_day(self):
+        date_now = datetime.now()
+        start_date = date_now.replace(hour=0, minute=00, second=00, microsecond=0)
+        end_date = date_now.replace(hour=23, minute=59, second=00, microsecond=9999)
     
-    def return_count_chamadas_by_day(self):
-        pass
+        return self.db.count_documents_by_day('chamadas', 'Data', start_date, end_date)
 
     def return_count_all_chamadas(self):
-        pass
+        return self.db.count_all_documents_on_collection('chamadas')
