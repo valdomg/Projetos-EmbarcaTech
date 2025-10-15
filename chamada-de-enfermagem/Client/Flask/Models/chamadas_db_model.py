7084df5db6379e48fc897e5402fac1e9bca54d64
Valdemiro <valdemiro128@outlook.com>
2025-10-15 17:21:23 -0300

    :sparkles: feat(ChamadasDBModel): implementando funções de retorno de chamadas por mês

--- chamada-de-enfermagem/Client/Flask/Models/chamadas_db_model.py
+++ chamada-de-enfermagem/Client/Flask/Models/chamadas_db_model.py
@@ -22,6 +22,12 @@ class ChamadasDBModel():
         end_date = date_now.replace(hour=23, minute=59, second=00, microsecond=9999)
     
         return self.db.count_documents_by_day('chamadas', 'Data', start_date, end_date)
+    
+    def return_chamadas_by_month(self, month:int):
+        pass
+
+    def return_number_of_chamadas_by_month(self, month:int):
+        pass
 
     def return_count_all_chamadas(self):
         return self.db.count_all_documents_on_collection('chamadas')
