4e83b881a58fd5b2de958ecd3d4b0e0119b252e0
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-10-21 17:10:35 -0300

    :recycle: refactor(monitoriamento-temperatura): atualização do arquivo de configuraçao do microcontrolador

--- monitoramento-de-temperatura/microcontrolador/config.h
+++ monitoramento-de-temperatura/microcontrolador/config.h
@@ -18,31 +18,10 @@ const constexpr uint8_t PIN_LED = 12;           //pino para utilizaçao da led
 
 
 // -----------------------------------------------------------------------------
-// Constantes de configuração do MQTT
+// Constantes de configuração para rede do microcontrolador usada para acesso a configurações internas
 // -----------------------------------------------------------------------------
-
-/// Endereço do broker MQTT.
-constexpr char* MQTT_SERVER = "xxxxxxxxxxx";
-
-/// Usuário para autenticação no broker MQTT.
-constexpr char* MQTT_USER = "xxxxxxxxxxxx";
-
-/// Senha para autenticação no broker MQTT.
-constexpr char* MQTT_PASS = "xxxxxxxxxxxx";
-
-/// Tópico para publicação de dados de sensores (temperatura/umidade).
-constexpr char* MQTT_TOPIC_PUBLICATION_DATA = "xxxxxxxxxxxx/xxxxxxxxxxxx";
-
-/// Tópico para publicação de alertas.
-constexpr char* MQTT_TOPIC_PUBLICATION_ALERT = "xxxxxxxxxxxx/xxxxxxxxxxxx";
-
-/// Identificação única do dispositivo no broker MQTT.
-constexpr char* MQTT_DEVICE_ID = "xxxxxxxxxxxx";
-
-
-// -----------------------------------------------------------------------------
-constexpr const char* SSID_ACCESS_POINT = "xxxxxxxxxxxxx";
-constexpr const char* PASSWORD_ACCESS_POINT = "xxxxxxxxxxxx";
+constexpr const char* SSID_ACCESS_POINT = "xxxxxxxxxxxxx"; // Nome da Rede Wifi
+constexpr const char* PASSWORD_ACCESS_POINT = "xxxxxxxxxxxx"; // Senha da Rede Wifi
 
 
 #endif
