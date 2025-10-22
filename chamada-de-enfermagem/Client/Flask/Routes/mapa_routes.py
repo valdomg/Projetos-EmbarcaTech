31aa0ebd73772661d02761c2cb861b9a31fc755a
Valdemiro <valdemiro128@outlook.com>
2025-10-22 20:20:22 -0300

    :sparkles: feat(MapaRoutes): implementação de endpoints para estado dos dispositivos

--- chamada-de-enfermagem/Client/Flask/Routes/mapa_routes.py
+++ chamada-de-enfermagem/Client/Flask/Routes/mapa_routes.py
@@ -5,12 +5,37 @@ mapa_bp = Blueprint('mapa', __name__, url_prefix='/api/mapa')
 
 '''
 Rota de api para informar qual enfermaria está com emergência
+
+ROTAS
+/api/mapa/emergencia
+/api/mapa/oscioso
 '''
 
-mapa_bp.route('/', methods=['POST'])
-def mapa():
+salas_status = {}
+
+@mapa_bp.route('/emergencia', methods=['POST'])
+def emergencia():
     data = request.get_json()
+    room_number = data['room_number']
+    
+    logging.warning(f'Emergência no local: {room_number}')
 
-    logging.warning('Chamada em:', data)
+    salas_status[room_number] = 'emergencia'
 
-    return jsonify({'status', 'ok'}), 200
+    return jsonify({'local':room_number}), 200
+
+@mapa_bp.route('/osciosa', methods=['POST'])
+def oscioso():
+    data = request.get_json()
+    room_number = data['room_number']
+
+    logging.warning(f'Osciocidade no local: {room_number}')
+
+    salas_status[room_number] = 'oscioso'
+
+    return jsonify({'local':room_number}), 200
+
+@mapa_bp.route('/status', methods=['GET'])
+def get_status():
+    print(salas_status)
+    return jsonify(salas_status), 200
