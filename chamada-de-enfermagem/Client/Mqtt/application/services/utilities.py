208043f02a30b27a3282db3fd72b195671c862aa
Valdemiro <valdemiro128@outlook.com>
2025-10-23 17:30:36 -0300

    :recycle: refactor(Utilites): refatorando função para que fique mais bunita e de acordo com a api mapa

--- chamada-de-enfermagem/Client/Mqtt/application/services/utilities.py
+++ chamada-de-enfermagem/Client/Mqtt/application/services/utilities.py
@@ -2,29 +2,22 @@ import requests
 import logging
 
 API_URL = 'http://localhost:5000/api/mapa'
-def send_update_to_flask(room_number:int, estado:str):
+def send_update_to_flask(room_number:int, status:str):
     '''
-    Função para enviar estado do microcontrolador para o endpoint de estados
+    Função para enviar status do microcontrolador para o endpoint de statuss
     '''
-
     try:
         endpoint = None
 
-        if estado == 'emergencia':
-            endpoint = f"{API_URL}/emergencia"
-
-        elif estado == 'oscioso':
-            endpoint = f"{API_URL}/oscioso"
-            
-        else:
-            logging.warning(f'Estado desconhecido: {estado}')
+        if status != 'emergencia' and status != 'oscioso':
+            logging.warning(f'status desconhecido: {status}')
             return
 
-        payload = {'room_number': room_number, 'estado':estado}
+        payload = {'room_number': room_number, 'status':status}
         response = requests.post(endpoint, json=payload, timeout=3)
 
         if response.status_code == 200:
-            logging.info(f'Sucesso ao enviar status do quarto {room_number} ({estado}) para o mapa.')
+            logging.info(f'Sucesso ao enviar status do quarto {room_number} ({status}) para o mapa.')
         else:
             logging.warning(f'Erro ao enviar status para o mapa: {response.status_code} - {response.text}')
 
