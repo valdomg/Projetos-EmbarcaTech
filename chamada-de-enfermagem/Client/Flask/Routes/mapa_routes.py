7fda3349d62574293c2b2ae1c944647abd1a3087
Valdemiro <valdemiro128@outlook.com>
2025-10-23 17:21:42 -0300

    :recycle: refactor(MapaRoutes): melhorando a lógica de api do mapa

--- chamada-de-enfermagem/Client/Flask/Routes/mapa_routes.py
+++ chamada-de-enfermagem/Client/Flask/Routes/mapa_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 import logging
+from Flask.Models.chamadas_status_model import ChamadasStatusModel
 
 mapa_bp = Blueprint('mapa', __name__, url_prefix='/api/mapa')
 
@@ -7,35 +8,43 @@ mapa_bp = Blueprint('mapa', __name__, url_prefix='/api/mapa')
 Rota de api para informar qual enfermaria está com emergência
 
 ROTAS
-/api/mapa/emergencia
-/api/mapa/oscioso
+/api/mapa/registrar_chamadas
+/api/mapa/status/oscioso
+/api/mapa/status/emergencia
+/api/mapa/status
 '''
 
-salas_status = {}
+status_chamadas = ChamadasStatusModel(list())
 
-@mapa_bp.route('/emergencia', methods=['POST'])
-def emergencia():
+@mapa_bp.route('/registrar', methods=['POST'])
+def registrar_chamada():
+
     data = request.get_json()
-    room_number = data['room_number']
-    
-    logging.warning(f'Emergência no local: {room_number}')
+    try:
+        if 'room_number' not in data or 'status' not in data:
+            return jsonify({'Erro':'Campos incorretos'}), 400
+        
+        if status_chamadas.adicionar_sala(data['room_number'], data['status']):
+            return jsonify({'Message': 'chamado registrado'}), 201
 
-    salas_status[room_number] = 'emergencia'
-
-    return jsonify({'local':room_number}), 200
-
-@mapa_bp.route('/osciosa', methods=['POST'])
-def oscioso():
-    data = request.get_json()
-    room_number = data['room_number']
-
-    logging.warning(f'Osciocidade no local: {room_number}')
-
-    salas_status[room_number] = 'oscioso'
-
-    return jsonify({'local':room_number}), 200
+    except Exception as e:
+        print(e)
 
 @mapa_bp.route('/status', methods=['GET'])
 def get_status():
-    print(salas_status)
-    return jsonify(salas_status), 200
+    print(status_chamadas.return_salas())
+    return jsonify(status_chamadas.return_salas()), 200
+
+
+@mapa_bp.route('/status/oscioso', methods=['GET'])
+def get_status_oscioso():
+    salas = status_chamadas.return_salas_status_oscioso()
+    return jsonify(salas), 200
+
+@mapa_bp.route('/status/emergencia', methods=['GET'])
+def get_status_emergencia():
+    salas = status_chamadas.return_salas_status_emergencia()
+    return jsonify(salas), 200
+
+
+
