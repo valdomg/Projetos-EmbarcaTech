f163c3725653612e89a8cb17934fce7e074e61cb
Valdemiro <valdemiro128@outlook.com>
2025-10-27 11:30:15 -0300

    :bug: fix(Utilites): corrigindo bug em que a url era None

--- chamada-de-enfermagem/Client/Mqtt/application/services/utilities.py
+++ chamada-de-enfermagem/Client/Mqtt/application/services/utilities.py
@@ -1,22 +1,21 @@
 import requests
 import logging
 
-API_URL = 'http://localhost:5000/api/mapa'
+API_URL = 'http://127.0.0.1:5000/api/mapa/registrar'
 def send_update_to_flask(room_number:int, status:str):
     '''
     Função para enviar status do microcontrolador para o endpoint de statuss
     '''
     try:
-        endpoint = None
 
         if status != 'emergencia' and status != 'oscioso':
             logging.warning(f'status desconhecido: {status}')
             return
 
         payload = {'room_number': room_number, 'status':status}
-        response = requests.post(endpoint, json=payload, timeout=3)
+        response = requests.post(API_URL, json=payload, timeout=3)
 
-        if response.status_code == 200:
+        if response.status_code == 201:
             logging.info(f'Sucesso ao enviar status do quarto {room_number} ({status}) para o mapa.')
         else:
             logging.warning(f'Erro ao enviar status para o mapa: {response.status_code} - {response.text}')
