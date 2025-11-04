c44f5446d66caac379b5c9c636dfad4315c984c6
Valdemiro <valdemiro128@outlook.com>
2025-11-04 17:29:02 -0300

    :sparkles: feat(Chamadas Route/Model) implementação de função e rota para contagem de ocorrências por dia no mês

--- chamada-de-enfermagem/Client/Flask/Models/chamadas_db_model.py
+++ chamada-de-enfermagem/Client/Flask/Models/chamadas_db_model.py
@@ -1,5 +1,6 @@
 from MongoDB.MongoDBConnection import MongoDBConnection
 from datetime import datetime, timezone
+import calendar
 
 class ChamadasDBModel():
     
@@ -36,6 +37,19 @@ class ChamadasDBModel():
         end_date = date_now.replace(day=31, month=month, hour=23, minute=59, second=00, microsecond=9999)
     
         return self.db.count_documents_by_date('chamadas', 'data', start_date, end_date)
+    
+    def return_count_chamadas_day_in_month(self, year_param=None, month_param=None):
+        date_now = datetime.now()
+    
+        if month_param:
+            date_now = date_now.replace(month=month_param)
+
+        if year_param:
+            date_now = date_now.replace(year=year_param)
+
+        _, month_days = calendar.monthrange(date_now.year, date_now.month)
+
+        return self.db.return_dict_with_all_documents_per_day('chamadas', 'data', date_now,  date_now.month, month_days)
 
     def return_count_all_chamadas(self):
         return self.db.count_all_documents_on_collection('chamadas')
