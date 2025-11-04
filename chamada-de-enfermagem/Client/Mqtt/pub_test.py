7c998673b451a6760c675ad2c043c9e9b51ab79e
Valdemiro <valdemiro128@outlook.com>
2025-11-04 10:42:16 -0300

    :recycle: refactor(Pub_test): adequando o pub_test para os novos tópicos e payload

--- chamada-de-enfermagem/Client/Mqtt/pub_test.py
+++ chamada-de-enfermagem/Client/Mqtt/pub_test.py
@@ -1,65 +1,76 @@
 import json
 import paho.mqtt.client as mqtt
+from time import sleep
 
-BROKER_HOST = 'localhost'
+BROKER_HOST = "localhost"
 BROKER_PORT = 1883
-id_test = 'pub_test'
-pass_test = '123'
+id_test = "pub_test"
+pass_test = "123"
 
 def publish_message(topic: str, payload: dict):
     """Publica uma mensagem MQTT no tópico especificado."""
     client = mqtt.Client(client_id=id_test)
     client.username_pw_set(id_test, pass_test)
     client.connect(BROKER_HOST, BROKER_PORT)
-    client.publish(topic, json.dumps(payload))
+
+    payload_f = json.dumps(payload, indent=4) 
+    print(payload_f)
+
+    client.publish(topic, payload_f)
     client.disconnect()
-    print(f"Mensagem publicada em '{topic}': {payload}")
+    print(f'Mensagem publicada em {topic}: {payload}')
 
 def pub_posto_enfermaria_de_enfermaria(dispositivo_id: str, estado: str, mensagem: str, room_number: str, local:str, comando: str):
     """Publica um evento vindo da enfermaria."""
-    topic = f'dispositivos/posto_enfermaria/{dispositivo_id}'
+    topic = f"dispositivos/posto_enfermaria"
+
     payload = {
-        'id': dispositivo_id,
-        'estado': estado,
-        'mensagem': mensagem,
-        'room_number': room_number,
-        'local': local,
-        'comando': comando
+        "id": dispositivo_id,
+        "estado": estado,
+        "mensagem": mensagem,
+        "room_number": room_number,
+        "local": local,
+        "comando": comando
     }
+
     publish_message(topic, payload)
 
 def pub_enfermaria_de_posto_enfermaria(dispositivo_id: str, estado: str, mensagem: str, room_number: str, local:str, comando: str):
     """Publica um evento vindo do posto de enfermaria no tópico local."""
-    topic = f'dispositivos/enfermaria/{dispositivo_id}'
+    topic = f"dispositivos/enfermaria/{dispositivo_id}"
+
     payload = {
-        'id': dispositivo_id,
-        'estado': estado,
-        'mensagem': mensagem,
-        'room_number': room_number,
-        'local': local,
-        'comando': comando
+        "id": dispositivo_id,
+        "estado": estado,
+        "mensagem": mensagem,
+        "room_number": room_number,
+        "local": local,
+        "comando": comando
     }
+
     publish_message(topic, payload)
 
 # ====== EXEMPLOS DE USO ======
 pub_posto_enfermaria_de_enfermaria(
 
-        dispositivo_id='enfermaria2',
-        estado='emergencia',
-        mensagem='ligar LED',
-        room_number='1',
-        local='enfermaria',
-        comando='ligar'
+        dispositivo_id="enfermaria1",
+        estado="emergencia",
+        mensagem="ligar LED",
+        room_number="1",
+        local="enfermaria",
+        comando="ligar"
         
 )
 
-'''
+sleep(20)
+
+
 pub_enfermaria_de_posto_enfermaria(
-        dispositivo_id='enfermaria10',
-        estado='oscioso',
-        mensagem='desligar LED',
-        room_number='10',
-        local='enfermaria',
-        comando='desligar'
+        dispositivo_id="enfermaria1",
+        estado="ocioso",
+        mensagem="desligar LED",
+        room_number="1",
+        local="enfermaria",
+        comando="desligar"
 )
-'''
+
