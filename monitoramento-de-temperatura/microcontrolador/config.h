f8dd75aeb933692ff449f0a9106beac7dfd5e4ef
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-11-05 15:17:05 -0300

    :wrench: build(monitoriamento-temperatura-config) traca dos pinos led e buzzer

--- monitoramento-de-temperatura/microcontrolador/config.h
+++ monitoramento-de-temperatura/microcontrolador/config.h
@@ -12,9 +12,9 @@ const constexpr uint8_t PIN_BUTTON_LONG = 2;   // botão long press
 const constexpr uint8_t PIN_SENSOR_SCL = 5;     //pino scl do sensor aht10
 const constexpr uint8_t PIN_SENSOR_SDA = 4;     //pino sda do sensor aht10
 
-const constexpr uint8_t PIN_BUZZER = 0;        //pino para atuação do buzzer
+const constexpr uint8_t PIN_BUZZER = 12;        //pino para atuação do buzzer
 
-const constexpr uint8_t PIN_LED = 12;           //pino para utilizaçao da led
+const constexpr uint8_t PIN_LED = 0;           //pino para utilizaçao da led
 
 
 // -----------------------------------------------------------------------------
