aa973d2c916c239e53fa0460cff66f6cf1f1e703
Valdemiro <valdemiro128@outlook.com>
2025-11-26 14:48:50 -0300

    :recycle: refactor(FlaskRoutes): refatoração de código na conexção com o banco de dados para que siga as recomendações da documentação do mongodb

--- chamada-de-enfermagem/Client/Flask/Routes/mapa_routes.py
+++ chamada-de-enfermagem/Client/Flask/Routes/mapa_routes.py
@@ -7,6 +7,7 @@ from Flask.auth import token_required, admin_required
 from Flask.Models.chamadas_status_model import ChamadasStatusModel
 from Flask.Services.convert_objectdID import convert_all_id_to_string, convert_object_id_to_string
 from MongoDB.MongoDBConnection import MongoDBConnection
+from MongoDB.mongo_conn import mongo_conn
 
 
 mapa_bp = Blueprint('mapa', __name__, url_prefix='/api/mapa')
@@ -20,22 +21,11 @@ ROTAS
 '''
 load_dotenv()
 
-uri = os.getenv('MONGO_URI')
-database = os.getenv('MONGO_DATABASE')
-
-mongo_conn = MongoDBConnection(uri, database)
 status_chamadas = ChamadasStatusModel(mongo_conn)
-
 
 @mapa_bp.route('/status', methods=['GET'])
 @token_required
 def get_status():
-
-    try:
-        mongo_conn.start_connection()
-    except Exception as e:
-        logging.exception('Erro ao conectar ao banco de dados')
-        return jsonify({'Error':'Erro interno do banco de dados'}), 500
 
     try:
         chamadas = status_chamadas.return_docs_from_status_chamadas()
@@ -48,21 +38,12 @@ def get_status():
         logging.exception('Error in return documents')
         return jsonify({'Error':'failure in return documents'}), 400
 
-    finally:
-        mongo_conn.close_connection()
-
     return {'Error': 'Chamadas não encontradas'},404
 
 
 @mapa_bp.route('/status/oscioso', methods=['GET'])
 @token_required
 def get_status_oscioso():
-
-    try:
-        mongo_conn.start_connection()
-    except Exception as e:
-        logging.exception('Erro ao conectar ao banco de dados')
-        return jsonify({'Error':'Erro interno do banco de dados'}), 500
 
     try:
         chamadas = status_chamadas.return_docs_status('oscioso')
@@ -74,21 +55,12 @@ def get_status_oscioso():
     except Exception as e:
         logging.exception('Error in return documents')
         return jsonify({'Error':'failure in return documents'}), 400
-
-    finally:
-        mongo_conn.close_connection()
-
+    
     return {'Error': 'Chamadas não encontradas'},404
 
 @mapa_bp.route('/status/emergencia', methods=['GET'])
 @token_required
 def get_status_emergencia():
-
-    try:
-        mongo_conn.start_connection()
-    except Exception as e:
-        logging.exception('Erro ao conectar ao banco de dados')
-        return jsonify({'Error':'Erro interno do banco de dados'}), 500
 
     try:
         chamadas = status_chamadas.return_docs_status('emergencia')
@@ -100,9 +72,6 @@ def get_status_emergencia():
     except Exception as e:
         logging.exception('Error in return documents')
         return jsonify({'Error':'failure in return documents'}), 400
-
-    finally:
-        mongo_conn.close_connection()
-
+    
     return {'Error': 'Chamadas não encontradas'},404
 
