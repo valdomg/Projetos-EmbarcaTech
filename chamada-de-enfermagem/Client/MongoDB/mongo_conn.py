597486926ddfced901fbb9e26b324cecdc2fb16d
Valdemiro <valdemiro128@outlook.com>
2025-11-26 14:55:46 -0300

    :sparkles: feat(MongoDB): adicionando arquivo de conexão global do mongodb

---
+++ chamada-de-enfermagem/Client/MongoDB/mongo_conn.py
@@ -0,0 +1,11 @@
+from MongoDB.MongoDBConnection import MongoDBConnection
+from dotenv import load_dotenv
+import os
+
+database = os.getenv('MONGO_DATABASE')
+uri = os.getenv('MONGO_URI')
+
+mongo_conn = MongoDBConnection(uri, database)
+mongo_conn.start_connection()
+
+
